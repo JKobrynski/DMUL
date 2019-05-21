1d8c235dbb4ba6c4b06584b80311f0db34e3190f
nlitkowski <norbert.litkowski@gmail.com>
2019-05-21 20:19:19 +0200

    Add svg logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Logo />
         <p>Norbert Litokoski, Julian Kobryński</p>
         <a
           className="App-link"
