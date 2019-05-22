b148e4f40c2df8f1dc4144532c21bfcb65b97f3d
JKobrynski <kobrynski@onet.pl>
2019-05-22 12:38:11 +0200

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Import components
+import Navbar from "./components/UI/Navbar";
+import Example from "./components/Playground/Example";
+import Landing from "./components/UI/Landing";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Norbert Litokoski, Julian Kobryński</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/example" component={Example} />
+        </div>
+        {/* <header className="App-header">
+          <p>Norbert Litokoski, Julian Kobryński</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button className="btn btn-dark">siusiak</button>
+        </header> */}
+      </div>
+    </Router>
   );
 }
 
