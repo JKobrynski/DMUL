5c4f984f6296e79c30c6de569e32d60f5d511d0c
JKobrynski <kobrynski@onet.pl>
2019-05-28 23:13:57 +0200

    React part finished

--- src/components/React/ReactNativePage.js
+++ src/components/React/ReactNativePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class ReactPage extends Component {
+export default class ReactNativePage extends Component {
   render() {
     return <div>React Native</div>;
   }
