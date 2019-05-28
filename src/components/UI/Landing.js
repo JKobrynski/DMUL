b2484f8461ccb58d0ece8d67c2805bf4b99aa832
nlitkowski <norbert.litkowski@gmail.com>
2019-05-28 22:15:26 +0200

    Add NN photo to landing page

--- src/components/UI/Landing.js
+++ src/components/UI/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import Logo from "./Logo";
+import DLImg from "../../assets/NNImages/DL.jpeg";
 
 export default class Landing extends Component {
   render() {
@@ -8,8 +9,17 @@ export default class Landing extends Component {
       <div className="container">
         <div className="row">
           <div className="col-md-9">
-            <div className="display-4 text-left">
+            <div className="display-4 mt-5 text-left">
               Informatyka jest fascynująca
+              <div className="row">
+                <a href="/nn">
+                  <img
+                    src={DLImg}
+                    className="LandingImage m-5"
+                    alt="Sieci neuronowe"
+                  />
+                </a>
+              </div>
             </div>
           </div>
           <div className="col-md-3">
